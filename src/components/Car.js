d1f15bbcc5bff28dd0c55a8ac0273ced17ba9fc7
Manimaran R <manimaraan.r@gmail.com>
2025-06-23 22:32:04 +0530

    component

---
+++ src/components/Car.js
@@ -0,0 +1,8 @@
+function Car()
+{
+  return(
+    <h2>Hello test this My Car component</h2>
+  )
+}
+
+export default Car;
---
+++ src/components/Garage.js
@@ -0,0 +1,14 @@
+import Car from "./Car";
+
+function Garage()
+{
+  return(
+    <>
+        <h2>Hello test this My Garage component</h2>
+        <Car />
+    </>
+    
+  )
+}
+
+export default Garage;
