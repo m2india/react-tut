fedc47958245dfed4ddc888fd6af927033900c85
Manimaran R <manimaraan.r@gmail.com>
2025-06-24 09:35:05 +0530

    toogle func

--- src/components/Apple.js
+++ src/components/Apple.js
@@ -4,9 +4,16 @@ class Apple extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-        color: "red",
-        size: "medium",
+            color: "red",
+            size: "medium",
+            mytest : {
+                name: "",
+                age: 30,
+            },
+            showMyTest: false, // Add this flag
         };
+
+        
     } 
     
     changeColor = () => {
@@ -16,6 +23,25 @@ class Apple extends React.Component {
     changeSize = () => {
         this.setState({ size: "large" });
     };
+
+    myTestFunction = () => {
+        this.setState({ 
+            mytest: {
+                ...this.state.mytest, name: 'Manimaran'
+            },
+            showMyTest: true, // Show when button clicked
+        });
+    };
+    
+    toggleMyTestFunction = () => {
+        this.setState((prevState) => ({
+            mytest: {
+                ...prevState.mytest,
+                name: prevState.showMyTest ? "" : "Manimaran", // Set name when showing, clear when hiding
+            },
+            showMyTest: !prevState.showMyTest, // Toggle flag
+        }));
+    };
     
     render() {
         return (
@@ -23,8 +49,13 @@ class Apple extends React.Component {
             <h1>Apple</h1>
             <p>Color: {this.state.color}</p>
             <p>Size: {this.state.size}</p>
+            {/* <h2>My function :{this.state.mytest.name}</h2> */}
+             {this.state.showMyTest && ( <h2>My function :{this.state.mytest.name}</h2>)}
             <button onClick={this.changeColor}>Change Color</button>
             <button onClick={this.changeSize}>Change Size</button>
+            <button onClick={this.toggleMyTestFunction}>
+                    {this.state.showMyTest ? "My Test" : "My Test"}
+                </button>
         </div>
         );
     }
